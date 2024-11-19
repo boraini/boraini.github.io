0cf0e9d879b0c77cb03354d689c2bcff9b441a05
boraini <boraini@users.noreply.github.com>
2024-11-19 18:21:33 +0000

    deploy: c282d3c8289533e4348fb6aa3bee93aa3e343654

---
+++ docs/_app/immutable/nodes/4.e2253b1f.js
@@ -0,0 +1,43 @@
+import{s as d,n as r}from"../chunks/scheduler.014bc20e.js";import{S as g,i as u,g as l,s as v,C as f,h,f as o,c as b,x as w,k as c,y as S,a as m}from"../chunks/index.74f4001b.js";function y(p){let e,a,t,s=`<section id="header" class="span-horizontal"><img src="/images/cv-profile.png" alt="Mert Bora İnevi"/> <h1>Mert Bora İnevi</h1> <p><a href="https://github.com/boraini" rel="nofollow">github.com/boraini</a></p> <p><a href="/portfolio/">boraini.com/portfolio</a></p></section> <section id="summary"><h1>Main Interests</h1> <h2>Web Development</h2> <p>I am especially proficient in Javascript and front-end development in general. You can find the source code of this website (which is developed with SvelteKit) and my other web projects on my Github account.</p> <h2>Embedded Systems Programming</h2> <p>I know how to make advanced devices using Arduino and its C++ dialect. In high school I have participated in the Robomasters AI Challenge as the engineer in charge of all the motion planning stuff and we worked with the ROS (Robot Operating System) where I programmed in Python.</p> <h2>Game Development</h2> <p>I have plenty of experience with Three.js and some with Unity 3D. Moreover, I am able to produce visual assets for my games using programs such as Blender 3D, Krita, Inkscape.</p></section> <section id="education"><h1>Education</h1> <ul><li><strong>Delft University of Technology</strong> BSc in Computer Science and Engineering, Systems Variant, 2021 - 2024</li> <li><strong>American Robert College of Istanbul</strong> 2016-2021</li> <li><strong>Oxbridge Advanced Studies Programme</strong> Engineering and Marketing Electives, 2018 Summer</li></ul></section> <section id="internship"><h1>Internship</h1> <p>At Sircle, we have developed a social media application used for organizing sports tournaments. I have worked on both the front end ant the back end.</p></section> <section id="experience"><h1>Non-Academic Experience and Voluntary Work</h1> <ul><li><strong>Antalya Coding Community Involvement Project</strong> We taught two groups of middle school students how to design and write computer programs and games. We used Scratch as our coding platform.</li> <li><strong>Kırşehir Coding Community Involvement Project</strong> We taught couples of middle school students how to design and write website front-ends.</li> <li><strong>RC Makers</strong> I have been a part of Robert College’s maker subculture club between 2016 and 2020.</li></ul></section> <section id="skills" class="span-vertical"><h1>Skills</h1> <h2>Server Development</h2> <p>Java, Scala, Spring Boot, Spring Hibernate, Node.js, Express.js, React, Svelte, Sass, Tailwind, MongoDB, PostgreSQL, Django</p> <h2>Python</h2> <p>PyGame, MatPlotLib, Python</p> <h2>Front End</h2> <p>JavaFX, HTML5, CSS3, JavaScript, TypeScript</p> <h2>Computer Graphics</h2> <p>OpenGL, C++, WebGPU</p> <h2>Mobile App Development</h2> <p>Flutter, React Native</p> <h2>Software Testing</h2> <p>Junit, Jest, PMD, Jqwik</p> <h2>Cloud and Serverless Computing</h2> <p>Docker, Vercel, Heroku, AWS, Linux Administration</p> <h2>Scripting</h2> <p>Bash, Python</p> <h2>DevOps</h2> <p>SSH, Bash, Git, GitLab (GitLab CI), GitHub (GitHub Workflows)</p> <h2>Language</h2> <ul><li>Turkish - Native</li> <li>English - C1 (IELTS, 2020)</li> <li>German - A2</li> <li>Dutch - A2</li></ul></section> <style lang="sass">main {
+  display: grid;
+  max-width: 80em;
+  grid-auto-flow: column;
+  grid-template-columns: repeat(2, minmax(30em, 1fr));
+  grid-template-rows: repeat(5, auto);
+  margin: auto;
+}
+@media (max-width: 60em) {
+  main {
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(9, auto);
+  }
+}
+main section {
+  margin: 1em;
+  border: 1px solid var(--page-foreground);
+  padding: 1em;
+  background-color: var(--page-background-spectacle);
+}
+main section.span-horizontal {
+  grid-column: 1/-1;
+}
+main section.span-vertical {
+  grid-row-end: span 4;
+}
+main section p {
+  margin-block: 0.4em;
+}
+main section h1 {
+  font-size: 1.5em;
+}
+main section h2 {
+  font-size: 1.2em;
+}
+main section#header img {
+  float: right;
+  height: 8em;
+  width: auto;
+}
+main section#header img::after {
+  display: block;
+}</style>`;return{c(){e=l("meta"),a=v(),t=l("main"),t.innerHTML=s,this.h()},l(n){const i=f("svelte-1ssk2ga",document.head);e=h(i,"META",{name:!0,content:!0}),i.forEach(o),a=b(n),t=h(n,"MAIN",{"data-svelte-h":!0}),w(t)!=="svelte-tgnrj6"&&(t.innerHTML=s),this.h()},h(){document.title="boraini.com - CV",c(e,"name","description"),c(e,"content","Mert Bora İnevi's CV")},m(n,i){S(document.head,e),m(n,a,i),m(n,t,i)},p:r,i:r,o:r,d(n){n&&(o(a),o(t)),o(e)}}}class I extends g{constructor(e){super(),u(this,e,null,y,d,{})}}export{I as component};
